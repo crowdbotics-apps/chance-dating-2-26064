7fc8b6638a914c940e37e8588234e22517290073
Crowdbotics <team@crowdbotics.com>
2021-05-01 20:26:36 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/chance_dating_2_26064/MainActivity.java
@@ -0,0 +1,15 @@
+package com.chance_dating_2_26064;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "chance_dating_2_26064";
+  }
+}
